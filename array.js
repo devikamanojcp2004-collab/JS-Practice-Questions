a5fb4db7d104f6960be23fd7ea1c68b0c8f261da
DEVIKA MANOJ C P <devikamanojcp2004@gmail.com>
2025-11-07 22:42:33 +0530

    js array beginner questions

---
+++ array.js
@@ -0,0 +1,28 @@
+//BEGINNER
+//(1)Create an array of 5 numbers and print the first and last element
+// let arr=[1,2,3,4,5]
+// console.log(arr[0],arr[4]);
+
+//(2)Find the *length* of an array
+// let arr=[2,67,97,9,80,99]
+// console.log(arr.length);
+
+//(3)Add a new element to the *end* and *beginning* of an array.
+// let arr=[2,67,97,9,80,99]
+// arr.push(33);
+// arr.unshift(10);
+// console.log(arr);
+
+//(4)Remove the *last* and *first* element from an array.
+// let arr=[2,67,97,9,80,99]
+// arr.pop(99);
+// arr.shift(2);
+// console.log(arr);
+
+
+//(5)Access the *3rd element* in an array and print it.
+//  let arr=[2,67,97,9,80,99]
+//  console.log(arr[2]);
+    
+
+
